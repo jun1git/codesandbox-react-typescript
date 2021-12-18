4680e2119eded26ba3787ec6957c4eb295d7eac8
jun1 <96167184+jun1git@users.noreply.github.com>
2021-12-19 00:43:45 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { TodoType } from "./types/todo";
+import { User } from "./types/user";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
 import "./styles.css";
 
+const user: User = {
+  name: "Smile",
+  hobbies: ["映画", "ゲーム"]
+};
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color={"red"} fontSize={"18px"} />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
       <Practice1 />
       <Practice2 />
       <Practice3 />
